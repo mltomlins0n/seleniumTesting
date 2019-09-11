07605e67014596a3d7d93d0a038575a7628f3747
mltomlins0n <martin070891@gmail.com>
2019-09-11 10:23:35 +0100

    Add test to navigate google images

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -3,6 +3,9 @@ package com.qa;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -19,14 +22,31 @@ public class GoogleSeleniumTest {
     }
 
     @After
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(3000);
+    public void tearDown() {
         driver.close();
     }
 
     @Test
-    public void searchTest() {
+    public void searchTest() throws InterruptedException {
         driver.manage().window().maximize();
         driver.get("http://www.google.com");
+        Thread.sleep(500);
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("funny cat pictures");
+        Thread.sleep(500);
+        searchField.sendKeys(Keys.ENTER);
+        //WebElement submitButton = driver.findElement(By.name("btnK"));
+        //submitButton.click();
+        Thread.sleep(500);
+        WebElement linkToTestingResult = driver.findElementByLinkText(
+                "Images for funny cat");
+        linkToTestingResult.click();
+        Thread.sleep(500);
+        WebElement catPic = driver.findElement(By.id("xY4xU-n5chBuNM:"));
+        catPic.click();
+        Thread.sleep(500);
+        WebElement page = driver.findElement(By.id("gsr")); // get html body
+        page.sendKeys(Keys.PAGE_DOWN);
+        Thread.sleep(2000);
     }
 }
