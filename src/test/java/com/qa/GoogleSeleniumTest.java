bf830a846426ac7b75133a551258d70550413063
mltomlins0n <martin070891@gmail.com>
2019-09-11 09:50:35 +0100

    Add GoogleSeleniumTest class

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -1,4 +1,30 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleSeleniumTest {
+
+    private ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty(
+                "web.chrome.driver",
+                "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void searchTest() {
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+    }
 }
