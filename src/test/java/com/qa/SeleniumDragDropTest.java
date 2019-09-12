6613b8a4b95d9371794e134150f89103f406148d
mltomlins0n <martin070891@gmail.com>
2019-09-12 14:35:31 +0100

    Add code to drag and drop class

--- src/test/java/com/qa/SeleniumDragDropTest.java
+++ src/test/java/com/qa/SeleniumDragDropTest.java
@@ -1,4 +1,48 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class SeleniumDragDropTest {
+
+    private ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "src/test/java/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void dragDropTest() throws InterruptedException {
+        driver.get("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+        //List<WebElement> elemList = driver.findElements(By.id("todrag"));
+
+        WebElement elemToDrag = driver.findElement(By.xpath("//*[@id=\"todrag\"]/span[1]"));
+        WebElement target = driver.findElement(By.id("mydropzone"));
+
+        Actions action = new Actions(driver);
+        Thread.sleep(1000);
+        action.dragAndDrop(elemToDrag, target).perform();
+
+        Thread.sleep(1000);
+
+    }
 }
