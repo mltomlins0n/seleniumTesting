ed27ce0e00a586a1840e475ab6a09e3b1bd87e76
Martin T <martin070891@gmail.com>
2020-05-29 13:24:55 +0100

    Use loops, added asserts

--- src/test/java/com/qa/DatePickerTests.java
+++ src/test/java/com/qa/DatePickerTests.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
+import static org.hamcrest.CoreMatchers.is;
 
 public class DatePickerTests {
   private WebDriver driver;
@@ -31,11 +31,11 @@ public class DatePickerTests {
 
   // Date Picker Tests
   @Test
-  public void specificDateTest() {
-    // Test name: Specific Date Test
+  public void todayTest() {
+    // Test name: Today Test
     driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
     driver.findElement(By.cssSelector(".date > .form-control")).click();
-    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(3)")).click();
+    driver.findElement(By.cssSelector(".datepicker-days tfoot .today")).click();
   }
   @Test
   public void monthPickerTest() {
@@ -59,28 +59,24 @@ public class DatePickerTests {
     driver.findElement(By.cssSelector("tr:nth-child(4) > .day:nth-child(3)")).click();
   }
   @Test
+  public void specificDateTest() {
+    // Test name: Specific Date Test
+    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
+    driver.findElement(By.cssSelector(".date > .form-control")).click();
+    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(3)")).click();
+  }
+  @Test
   public void backForthArrowsTestRange() {
     // Test name: Back/Forth Arrows Test (Range)
     driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
     driver.findElement(By.cssSelector(".date > .form-control")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    {
-      WebElement element = driver.findElement(By.cssSelector(".datepicker-days .prev"));
-      Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
+    WebElement backArrow = driver.findElement(By.cssSelector(".datepicker-days .prev"));
+    WebElement forwardArrow = driver.findElement(By.cssSelector(".datepicker-days .next"));
+    for (int i = 0; i < 6; i++) {
+      backArrow.click();
+      forwardArrow.click();
     }
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .next")).click();
     driver.findElement(By.cssSelector("tr:nth-child(1) > .day:nth-child(6)")).click();
-  }
-  @Test
-  public void todayTest() {
-    // Test name: Today Test
-    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
-    driver.findElement(By.cssSelector(".date > .form-control")).click();
-    driver.findElement(By.cssSelector(".datepicker-days tfoot .today")).click();
   }
   @Test
   public void clearButtonTest() {
@@ -90,36 +86,11 @@ public class DatePickerTests {
     driver.findElement(By.cssSelector(".datepicker-days tfoot .today")).click();
     driver.findElement(By.cssSelector(".date > .form-control")).click();
     driver.findElement(By.cssSelector(".datepicker-days .clear")).click();
+    // Verify that field is empty
+    assertThat(driver.findElement(By.cssSelector("#sandbox-container1 > div > input")).getText(), is (""));
   }
 
   // Date Range Picker Tests
-  @Test
-  public void backForthArrowsTest() {
-    // Test name: Back/Forth Arrows Test
-    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
-    driver.findElement(By.cssSelector("#datepicker > .form-control:nth-child(1)")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    {
-    WebElement element = driver.findElement(By.cssSelector(".datepicker-days .prev"));
-    Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
-    }
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .next")).click();
-    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(2)")).click();
-    driver.findElement(By.cssSelector(".form-control:nth-child(3)")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .next")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .next")).click();
-    {
-      WebElement element = driver.findElement(By.cssSelector(".datepicker-days .next"));
-      Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
-    }
-    driver.findElement(By.cssSelector(".datepicker-days .next")).click();
-    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
-    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(2)")).click();
-  }
   @Test
   public void oneDayRangeTest() {
     // Test name: One Day Range Test
@@ -127,6 +98,24 @@ public class DatePickerTests {
     driver.findElement(By.cssSelector("#datepicker > .form-control:nth-child(1)")).click();
     driver.findElement(By.cssSelector("tr:nth-child(4) > .day:nth-child(4)")).click();
     driver.findElement(By.cssSelector(".row:nth-child(3) .panel-body")).click();
+    // Check the value of the field
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(3)"))
+    .getAttribute("value"), is ("20/05/2020"));
+  }
+  @Test
+  public void oneWeekRangeTest() {
+    // Test name: One Week Range Test
+    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
+    driver.findElement(By.cssSelector("#datepicker > .form-control:nth-child(1)")).click();
+    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(4)")).click();
+    driver.findElement(By.cssSelector(".form-control:nth-child(3)")).click();
+    driver.findElement(By.cssSelector("tr:nth-child(3) > .day:nth-child(4)")).click();
+    // Check that each field has the correct values
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(1)"))
+    .getAttribute("value"), is ("06/05/2020"));
+
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(3)"))
+    .getAttribute("value"), is ("13/05/2020"));
   }
   @Test
   public void oneMonthRangeTest() {
@@ -140,15 +129,12 @@ public class DatePickerTests {
     driver.findElement(By.cssSelector(".datepicker-days .datepicker-switch")).click();
     driver.findElement(By.cssSelector(".month:nth-child(5)")).click();
     driver.findElement(By.cssSelector("tr:nth-child(1) > .day:nth-child(6)")).click();
-  }
-  @Test
-  public void oneWeekRangeTest() {
-    // Test name: One Week Range Test
-    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
-    driver.findElement(By.cssSelector("#datepicker > .form-control:nth-child(1)")).click();
-    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(4)")).click();
-    driver.findElement(By.cssSelector(".form-control:nth-child(3)")).click();
-    driver.findElement(By.cssSelector("tr:nth-child(3) > .day:nth-child(4)")).click();
+    // Check that each field has the correct value
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(1)"))
+    .getAttribute("value"), is ("01/04/2020"));
+
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(3)"))
+    .getAttribute("value"), is ("01/05/2020"));
   }
   @Test
   public void oneYearRangeTest() {
@@ -166,5 +152,35 @@ public class DatePickerTests {
     driver.findElement(By.cssSelector(".year:nth-child(12)")).click();
     driver.findElement(By.cssSelector(".month:nth-child(1)")).click();
     driver.findElement(By.cssSelector("tr:nth-child(1) > .day:nth-child(4)")).click();
+    // Check that each field has the correct values
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(1)"))
+    .getAttribute("value"), is ("01/01/2019"));
+
+    assertThat(driver.findElement(By.cssSelector("#datepicker > input:nth-child(3)"))
+    .getAttribute("value"), is ("01/01/2020"));
+  }
+  @Test
+  public void backForthArrowsTest() {
+    // Test name: Back/Forth Arrows Test
+    driver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
+    driver.findElement(By.cssSelector("#datepicker > .form-control:nth-child(1)")).click();
+    WebElement backArrow = driver.findElement(By.cssSelector(".datepicker-days .prev"));
+    WebElement forwardArrow = driver.findElement(By.cssSelector(".datepicker-days .next"));
+    
+    for (int i = 0; i < 5; i++) {
+      backArrow.click();
+    }
+
+    forwardArrow.click();
+    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(2)")).click();
+    driver.findElement(By.cssSelector(".form-control:nth-child(3)")).click();
+    WebElement forwardArrow2 = driver.findElement(By.cssSelector(".datepicker-days .next"));
+
+    for (int i = 0; i < 5; i++) {
+      forwardArrow2.click();
+    }
+
+    driver.findElement(By.cssSelector(".datepicker-days .prev")).click();
+    driver.findElement(By.cssSelector("tr:nth-child(2) > .day:nth-child(2)")).click();
   }
 }
