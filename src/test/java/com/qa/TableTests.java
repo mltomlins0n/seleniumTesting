713bfb002ef4cc52c31bc4ef2ad861434d074739
Martin T <martin070891@gmail.com>
2020-08-18 18:46:00 +0100

    Add PaginationTest()

--- src/test/java/com/qa/TableTests.java
+++ src/test/java/com/qa/TableTests.java
@@ -1,10 +1,14 @@
 package com.qa;
 
+import java.util.List;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.After;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class TableTests {
     private WebDriver driver;
@@ -22,5 +26,25 @@ public class TableTests {
     @After
     public void tearDown() {
       driver.quit();
+    }
+
+    // Table Tests
+    @Test
+    public void PaginationTest() {
+        driver.get("https://www.seleniumeasy.com/test/table-pagination-demo.html");
+        driver.findElement(By.linkText("»")).click();
+
+        List<WebElement> elements = driver.findElements(By.linkText("«"));
+        assert(elements.size() > 0);
+
+        driver.findElement(By.linkText("»")).click();
+
+        assert(elements.size() > 0);
+
+        driver.findElement(By.linkText("«")).click();
+        driver.findElement(By.linkText("2")).click();
+        driver.findElement(By.linkText("3")).click();
+        driver.findElement(By.linkText("2")).click();
+        driver.findElement(By.linkText("1")).click();
     }
 }
