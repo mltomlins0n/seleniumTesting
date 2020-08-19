ec4004b848f333dc48c264bf284967dc24da12fb
Martin T <martin070891@gmail.com>
2020-08-19 15:58:48 +0100

    Rename tableSearch1() top filterNumber() and add logic to assert no. of results

--- src/test/java/com/qa/TableTests.java
+++ src/test/java/com/qa/TableTests.java
@@ -1,11 +1,12 @@
 package com.qa;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
+import static org.junit.Assert.assertNotNull;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -49,20 +50,13 @@ public class TableTests {
         driver.findElement(By.linkText("2")).click();
         driver.findElement(By.linkText("1")).click();
     }
-
     @Test
-    public void tableSearch1() {
+    public void filterNumber() {
         driver.get("https://www.seleniumeasy.com/test/table-search-filter-demo.html");
 
         WebElement table1 = driver.findElement(By.id("task-table"));
         WebElement table1Body = table1.findElement(By.tagName("tbody"));
         List<WebElement> tableRows = table1Body.findElements(By.tagName("tr"));
-
-        //int trCount = 0;
-        //for(WebElement tr : tableRows) {
-        //    trCount++;
-        //    System.out.println("table data: " + trCount + tr);
-        //}
 
         WebElement searchField = driver.findElement(By.id("task-table-filter"));
         searchField.click();
@@ -70,6 +64,15 @@ public class TableTests {
         // assert that only one row is visible
         WebElement result = driver.findElement(By.cssSelector("#task-table > tbody > tr:nth-child(1)"));
         assertNotNull(result);
+        // New list to store the query results
+        List<WebElement> tableResults = new ArrayList<>();
+        for(WebElement tr : tableRows) {
+          if (tr.isDisplayed()) {
+            tableResults.add(tr);
+            System.out.println("table data: " + tableResults);
+          }
+        }
+        assert tableResults.size() == 1;
     }
 
 }
