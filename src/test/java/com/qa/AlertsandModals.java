553310a930124de4d84e0cf9e00c5c12d9dddd97
Martin T <martin070891@gmail.com>
2020-10-15 16:45:53 +0100

    Add bootstrap modals function

--- src/test/java/com/qa/AlertsandModals.java
+++ src/test/java/com/qa/AlertsandModals.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import static org.junit.Assert.assertFalse;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -61,5 +60,44 @@ public class AlertsandModals {
             assertFalse("The alerts should be gone", alert.isDisplayed());
         }
     }
-    
+    @Test
+    public void bootstrapModals() {
+        driver.get("https://www.seleniumeasy.com/test/bootstrap-modal-demo.html");
+        WebElement launchButton = driver.findElement(By.cssSelector(
+            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div:nth-child(2) > div > div > div.panel-body > a"));
+        launchButton.click();
+        WebElement modal = driver.findElement(By.cssSelector(
+            "#myModal0 > div > div > div.modal-footer"));
+        wait.until(ExpectedConditions.visibilityOf(modal));
+        // Click the top right close icon
+        driver.findElement(By.className("close")).click();
+        launchButton.click();
+        wait.until(ExpectedConditions.visibilityOf(modal));
+        // Click the close button
+        WebElement closeButton = driver.findElement(By.className("btn"));
+        // Using js executor to get around "Element Click Intercepted" exception
+        js.executeScript("arguments[0].click()", closeButton);
+        launchButton.click();
+        wait.until(ExpectedConditions.visibilityOf(modal));
+        // Click the "Save Changes" button
+        WebElement saveButton = driver.findElement(By.xpath("//*[@id=\"myModal0\"]/div/div/div[4]/a[2]"));
+        js.executeScript("arguments[0].click()", saveButton);
+
+        // TODO - Use this switch statement to automate the modal
+        for (int i = 0; i < 3; i++) {
+            switch(i) {
+                case 0:
+                System.out.println(i);
+                break;
+                case 1:
+                System.out.println("case 1 " + i);
+                break;
+                case 2:
+                System.out.println("Case 2 " + i);
+                break;
+                default:
+                System.out.println("You shouldn't be here...");
+            }
+        }
+    }
 }
