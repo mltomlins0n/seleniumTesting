c57b6fcd347cd38dc4eaa87e49c7d63dcfe2fe35
Martin T <martin070891@gmail.com>
2020-10-15 15:39:47 +0100

    Change bootstrap alerts function to use loops, added asserts

--- src/test/java/com/qa/AlertsandModals.java
+++ src/test/java/com/qa/AlertsandModals.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import static org.junit.Assert.assertFalse;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -46,39 +47,17 @@ public class AlertsandModals {
         List <WebElement> alerts = alertContainer.findElements(By.tagName("div"));
 
         for (WebElement button : buttons) {
-            // Click each button, close each alert
-            // Wait for the autoclose alerts to be closed
             button.click();
         }
-        // I tried looping through a list of alerts and clicking all the buttons
-        // by selecting all <divs>
-        // The only way I can select alert buttons is through XPath, so no loops
-        WebElement successCloseButton = driver.findElement(By.xpath(
-            "/html/body/div[2]/div/div[2]/div/div[2]/div[2]/button"));
-        wait.until(ExpectedConditions.elementToBeClickable(successCloseButton));
-        successCloseButton.click();
-
-        WebElement warningCloseButton = driver.findElement(By.xpath(
-            "/html/body/div[2]/div/div[2]/div/div[2]/div[4]/button"));
-        wait.until(ExpectedConditions.elementToBeClickable(warningCloseButton));
-        warningCloseButton.click();
-
-        WebElement dangerCloseButton = driver.findElement(By.xpath(
-            "/html/body/div[2]/div/div[2]/div/div[2]/div[6]/button"));
-        wait.until(ExpectedConditions.elementToBeClickable(dangerCloseButton));
-        dangerCloseButton.click();
-
-        WebElement infoCloseButton = driver.findElement(By.xpath(
-            "/html/body/div[2]/div/div[2]/div/div[2]/div[8]/button"));
-        wait.until(ExpectedConditions.elementToBeClickable(infoCloseButton));
-        infoCloseButton.click();
-        
+        // Loop through alerts and if they aren't autoclose ones, click the close button
         for (WebElement alert : alerts) {
-            // TODO - wait for alerts to be invis and assert
+            // Select only the alerts that don't auto close
+            if (alert.getAttribute("hidden") != null) {
+                WebElement  closeButton = alert.findElement(By.tagName("button"));
+                wait.until(ExpectedConditions.elementToBeClickable(closeButton)).click();
+            }
+            wait.until(ExpectedConditions.invisibilityOf(alert));
+            assertFalse("The alerts should be gone", alert.isDisplayed());
         }
-        WebElement successAlert = driver.findElement(By.cssSelector(
-            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div > div.col-md-6 > div.alert.alert-success.alert-autocloseable-success"));
-            wait.until(ExpectedConditions.invisibilityOf(successAlert));
-        assertFalse("Alerts should be gone", successAlert.isDisplayed());
     }
 }
