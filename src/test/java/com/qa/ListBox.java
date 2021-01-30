f32f11e3fc6fa24270a3ee38b778dce3532b242e
Martin T <martin070891@gmail.com>
2021-01-30 14:26:47 +0000

    Add left and right check all functions

--- src/test/java/com/qa/ListBox.java
+++ src/test/java/com/qa/ListBox.java
@@ -75,14 +75,76 @@ public class ListBox {
         WebElement rightListBox = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[3]/div/ul"));
         List <WebElement> rightList = rightListBox.findElements(By.className("list-group-item"));
 
-        WebElement rightAllButton = driver.findElement(By.cssSelector("#listhead > div.col-md-2 > div > a"));
-        WebElement leftAllButton = driver.findElement(By.cssSelector("#listhead > div.col-md-2 > div > a"));
         WebElement leftSearchBox = driver.findElement(By.cssSelector("#listhead > div.col-md-10 > div > input"));
         WebElement rightSearchBox = driver.findElement(By.cssSelector("#listhead > div.col-md-10 > div > input"));
 
         for (WebElement item : rightList) {
             item.click();
         }
+        moveLeftButton.click();
+        List <WebElement> rightList2 = rightListBox.findElements(By.className("list-group-item"));
+        assert(rightList2.isEmpty());
+        moveRightButton.click();
+        List <WebElement> rightList3 = rightListBox.findElements(By.className("list-group-item"));
+        assert(rightList3.size()==5);
+
+        ReentrantLock lock = new ReentrantLock();
+        synchronized(rightList) {
+            try {
+                lock.lock();
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    @Test
+    public void leftCheckAll() {
+        WebElement moveLeftButton = driver.findElement(By.cssSelector(
+            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div > div.list-arrows.col-md-1.text-center > button.btn.btn-default.btn-sm.move-left"));
+        WebElement moveRightButton = driver.findElement(By.cssSelector(
+            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div > div.list-arrows.col-md-1.text-center > button.btn.btn-default.btn-sm.move-right"));
+
+        WebElement leftListBox = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[1]/div/ul"));
+        List <WebElement> leftList = leftListBox.findElements(By.className("list-group-item"));
+        WebElement leftAllButton = driver.findElement(By.cssSelector("#listhead > div.col-md-2 > div > a"));
+        
+        leftAllButton.click();
+        moveRightButton.click();
+        List <WebElement> leftList2 = leftListBox.findElements(By.className("list-group-item"));
+        assert(leftList2.isEmpty());
+        moveLeftButton.click();
+        List <WebElement> leftList3 = leftListBox.findElements(By.className("list-group-item"));
+        assert(leftList3.size()==5);
+
+        ReentrantLock lock = new ReentrantLock();
+        synchronized(leftList) {
+            try {
+                lock.lock();
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    @Test
+    public void rightCheckAll() {
+        WebElement moveLeftButton = driver.findElement(By.cssSelector(
+            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div > div.list-arrows.col-md-1.text-center > button.btn.btn-default.btn-sm.move-left"));
+        WebElement moveRightButton = driver.findElement(By.cssSelector(
+            "body > div.container-fluid.text-center > div > div.col-md-6.text-left > div > div.list-arrows.col-md-1.text-center > button.btn.btn-default.btn-sm.move-right"));
+
+        WebElement rightListBox = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[3]/div/ul"));
+        List <WebElement> rightList = rightListBox.findElements(By.className("list-group-item"));
+        WebElement rightAllButton = driver.findElement(By.xpath("//*[@id='listhead']/div[1]/div/a"));
+        
+        rightAllButton.click();
         moveLeftButton.click();
         List <WebElement> rightList2 = rightListBox.findElements(By.className("list-group-item"));
         assert(rightList2.isEmpty());
